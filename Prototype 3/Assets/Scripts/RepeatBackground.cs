ccf0f8265874d731571b6b1f56730f574ceb6c9b
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-01-31 11:58:56 -0800

    3.2.3 c

--- Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < startPos.x - 50)
+        {
+            transform.position = startPos; 
+        }
     }
 }
